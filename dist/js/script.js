b14e1efe1f4f3036fc4c2522461089891283a774
Serhii <sergiomakafon@gmail.com>
2022-06-02 19:05:50 +0300

    Full page completed (desktop version only)

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.menu__close');
+      menuLink = document.querySelectorAll('.menu__link');
+
 
 hamburger.addEventListener('click', () => {
    menu.classList.add('active');
@@ -10,14 +12,22 @@ close.addEventListener('click', () => {
    menu.classList.remove('active');
 });
 
+menuLink.forEach(item => {
+   item.addEventListener('click', () => {
+      menu.classList.remove('active');
+   });
+});
+
 window.addEventListener("keydown", (e) => {
    if (e.key == 'Escape') {
        menu.classList.remove('active');
    }
 });
 
-window.addEventListener('click', (e) => {
-   if (e.target == overlay) {
-       menu.classList.remove('active');
-   }
+const percent = document.querySelectorAll('.counter__percent'),
+      lines = document.querySelectorAll('.counter__line span');
+
+percent.forEach((item, i) => {
+   lines[i].style.width = item.innerHTML;
 });
+
