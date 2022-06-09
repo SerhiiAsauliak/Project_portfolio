db66295d1c1a415c13c24162e892673e0cc7a476
Serhii <sergiomakafon@gmail.com>
2022-06-09 12:56:28 +0300

    Full page with mobile adaptation (all)

--- dist/js/script.js
+++ dist/js/script.js
@@ -31,3 +31,17 @@ percent.forEach((item, i) => {
    lines[i].style.width = item.innerHTML;
 });
 
+$(document).ready(function(){
+   $(window).scroll(function() {
+      if ($(this).scrollTop() >1000){
+      $('.pageup').fadeIn();
+      } else {
+         $('.pageup').fadeOut();}
+   });
+
+   $("a[href=#up]").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+    });
+});
